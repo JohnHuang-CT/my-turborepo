d4bb66c5fa2611a68c5216bf17a4ddd314deebb4
johnhuang <hjp1.@meitu.com>
2024-11-01 14:18:36 +0800

    123

---
+++ packages/ts-utils/.eslintrc.js
@@ -0,0 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+  extends: ["@weber-jojo/eslint-config/react.js"],
+};
--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -1,9 +1,13 @@
+import { add } from "@weber-jojo/ts-utils";
+
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode;
 }
 
 export function Button({ children, ...other }: ButtonProps): JSX.Element {
+  console.log(add());
+
   return (
     <button type="button" {...other}>
       {children}123123213asdasdfffasdasdadssd
