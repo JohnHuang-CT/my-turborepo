2c3dd735c107055bf7fb2202ee4fe1e7fa507c24
johnhuang <hjp1.@meitu.com>
2024-11-11 14:15:11 +0800

    123

---
+++ packages/release-it-pnpm/.release-it.cjs
@@ -0,0 +1,28 @@
+const version = "${version}";
+const packageName = process.env.npm_package_name;
+const scope = packageName.split("/")[1];
+
+module.exports = {
+  plugins: {
+    "../release-it-pnpm/index.js": {
+      disableRelease: true,
+    },
+
+    // https://github.com/release-it/keep-a-changelog
+    "./index.js": {
+      filename: "CHANGELOG.md",
+      strictLatest: false,
+      addUnreleased: true,
+    },
+  },
+  // https://github.com/release-it/release-it/blob/main/docs/git.md
+  git: {
+    tagName: `${packageName}@${version}`,
+    commitMessage: `feat(${scope}): released version v${version} [no ci]`,
+    requireCommits: true,
+    requireCommitsFail: false,
+    requireCleanWorkingDir: false,
+  },
+
+  npm: false,
+};
